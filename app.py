3839adaf89f41b35ccb790d5dab8f4be3b4381d7
BillRAI <150030700+BillRAI@users.noreply.github.com>
2024-11-07 00:11:29 +0700

    update

--- app.py
+++ app.py
@@ -125,4 +125,4 @@ def download_file(filename):
         return "Error sending file", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', debug=True, port=3000)
